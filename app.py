d4db6636282962ded96bc495a0bf7925a56e60a8
Mouad Guemati <mouad.gmati2015@gmail.com>
2026-07-21 08:50:06 +0200

    Correction orthographe import carousel

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import json
-from streamlit_caroussel import caroussel
+from image_caroussel import caroussel
 from data.hotels_data import HOTELS_DATA
 import streamlit as st
 import streamlit.components.v1 as components
